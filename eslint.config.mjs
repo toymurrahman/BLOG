dd2936e8f414e617d8856c41b653a50efc90febf
Tonmoy <rahmantoymur2@gmail.com>
2025-03-26 21:20:31 +0600

    Update eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import globals from "globals" ; 2 +import pluginJs from "@eslint/js" ; 
+import tseslint from "typescript-eslint"; 
+ @type {import ('eslint').Linter.Config[ }  
++export default
+{ files: ["**/*. {js, mjs, cjs, ts) " ] 
+ { language0ptions : { globals : globals.node }  ...tseslint. configs. recommended,
+ +];
