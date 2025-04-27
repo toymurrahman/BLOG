9a4d6de631ebe8e72009d4f5a6abd2303a51cfca
Tonmoy <rahmantoymur2@gmail.com>
2025-04-27 21:41:59 +0600

    Update src

---
+++ src/Blog.tsx
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+import { IBlog } from './blog.interface';
+
+const blogSchema = new Schema<IBlog>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Blog = model<IBlog>('Blog', blogSchema);
+export default Blog;
